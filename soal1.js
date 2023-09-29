cfce3142b5ebee5edd1ec68379406ff706175441
teddyadh <teddyadh24@gmail.com>
2023-09-29 13:35:06 +0700

    pengumpulan homework 7

---
+++ soal1.js
@@ -0,0 +1,8 @@
+const calc = require('./calc')
+
+
+console.log(calc.luasPersegi(10));
+console.log(calc.kelilingPersegi(2));
+
+console.log(calc.luasPersegiPanjang(10, 2));
+console.log(calc.kelilingPersegiPanjang(10, 2));
